cc33484d776b42e264b2f753a271b08d11b75877
Markus Morgenstern <markusmorgenstern87@outlook.de>
2021-04-28 16:32:32 +0200

    add typings

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 export class Client {
     constructor(url: string)
-    send(type, payload): void;
-    onMessage(cb): void;
-    onError(cb): void;
+    send<T>(type: string, payload: T): void;
+    onMessage<T>(cb: (a: {type: string, payload: T}) => void): void;
+    onError(cb: (e: Error) => void): void;
     connect(): Promise<void>;
     disconnect(): void;
 }
